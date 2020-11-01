80114d40c33a152d7c9371b7935c7248c4262f28
mohamad lounnas <mohamadlounnas@gmail.com>
2020-11-01 18:22:46 +0000

    Add files via upload

---
+++ docs/precache-manifest.03f4253fb6783febd23d54cf82c97843.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeffb553dedbfb4d2c87",
+    "url": "/css/app.8d20c3a9.css"
+  },
+  {
+    "revision": "824169000de8904dd51e4939c4e887d6",
+    "url": "/img/g1.82416900.webp"
+  },
+  {
+    "revision": "fdec84c268d3169483ee4f55c1cc0802",
+    "url": "/img/ge-cart.fdec84c2.png"
+  },
+  {
+    "revision": "b4e1a3584b8de72931b292da5a834939",
+    "url": "/img/pizza.b4e1a358.jpg"
+  },
+  {
+    "revision": "2812de3e3278679940b676ab01405901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f0195955b5478156f1",
+    "url": "/js/about.7ba75c23.js"
+  },
+  {
+    "revision": "eeffb553dedbfb4d2c87",
+    "url": "/js/app.09c08ebf.js"
+  },
+  {
+    "revision": "3c977e1ef3045d157568",
+    "url": "/js/chunk-vendors.e0ffae26.js"
+  },
+  {
+    "revision": "a7bb829f05709f04c4cc8f0bb0289d89",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
